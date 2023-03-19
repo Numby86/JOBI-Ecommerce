daba083c249c0b3b56d6bd3dfe3820eb2c290c63
Numby86 <numby86@gmail.com>
2023-03-19 20:37:58 +0100

    feat(JOBI)create loading

--- src/app/core/services/garments/garments.service.ts
+++ src/app/core/services/garments/garments.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { ApiGarmentsService } from './api/api-garments.service';
-import { Observable, map } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 import { ApiGarment } from './api/models/api-garment.models';
 import { Garment } from './models/garment.models';
 import { transformGarment} from './garments.helpers'
+import { LoadingService } from './../loading/loading.service';
 
 
 @Injectable({
@@ -12,22 +13,27 @@ import { transformGarment} from './garments.helpers'
 export class GarmentsService {
 
   constructor(
-    private apiGarmentsService: ApiGarmentsService
+    private apiGarmentsService: ApiGarmentsService,
+    private loadingService: LoadingService
   ) { }
 
   public getGarments(): Observable<Garment[]> {
+    this.loadingService.showLoading();
     return this.apiGarmentsService.getApiGarments().pipe(
       map((garments: ApiGarment[]) =>{
         return garments.map((garment) => transformGarment (garment));
-      })
+      }),
+      tap(() => this.loadingService.hideLoading())
     )
   };
 
   public getGarmentsDetail(id: string): Observable<Garment> {
+    this.loadingService.showLoading();
     return this.apiGarmentsService.getApiGarmentDetail(id).pipe(
       map((ApiGarment) => {
         return transformGarment(ApiGarment);
-      })
+      }),
+      tap(() => this.loadingService.hideLoading())
     )
   }
 
--- src/app/core/services/loading/loading.service.ts
+++ src/app/core/services/loading/loading.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoadingService {
 
-  constructor() { }
+  public shouldShowLoader$: ReplaySubject<boolean> = new ReplaySubject<boolean>();
+
+  constructor() { 
+
+    this.shouldShowLoader$.next(false);
+  }
+
+  public showLoading() {
+    this.shouldShowLoader$.next(true);
+  }
+
+  public hideLoading() {
+    this.shouldShowLoader$.next(false);
+  }
+
 }
