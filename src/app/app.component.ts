b3425b02297b841260f8fbca89a0ab39840d2aca
Isa-Hub-22 <isabel.raso@bootcamp-upgrade.com>
2023-03-21 18:35:18 +0100

    feat(JOBI) created themes light and dark

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    const userPrefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+
+    if (userPrefersDark) {
+      this.setThema('dark')
+    }
+    if (userPrefersLight) {
+      this.setThema('light')
+    }
+  }
   title = 'JOBI';
+  setThema(t: string) {
+   
+    document.documentElement.className = t;
+  }
+ 
 }
