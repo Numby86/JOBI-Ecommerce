a1d94b70cef02d1e9d1452acfdc7c94050145c4e
Numby86 <numby86@gmail.com>
2023-03-22 19:39:49 +0100

    feat(JOBI)changes router and buttons cart

--- src/app/pages/garment-detail/garment-detail.component.ts
+++ src/app/pages/garment-detail/garment-detail.component.ts
@@ -1,6 +1,6 @@
 import { Garment } from 'src/app/core/services/garments/models/garment.models';
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'src/app/core/services/products/models/product.models';
 import { ProductsService } from 'src/app/core/services/products/products.service';
 import { CartService } from 'src/app/core/services/cart/cart.service';
@@ -17,7 +17,8 @@ export class GarmentDetailComponent {
     constructor (
       private activatedRoute: ActivatedRoute,
       private productsService: ProductsService,
-      private cartService: CartService
+      private cartService: CartService,
+      private router: Router
     ){
       this.activatedRoute.params.subscribe((params) => {
         const garmentId = params['id'];
@@ -27,6 +28,10 @@ export class GarmentDetailComponent {
       })
     }
     
+    public backToList(){
+      this.router.navigate(['garment-list'])
+    }
+
     public addToCart(product: Product){
       return this.cartService.addProduct(product);
     }
